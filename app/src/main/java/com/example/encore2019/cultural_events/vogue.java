197144c5228a98eb4f8f0130769972790e879106
tkgoel213 <tkgoel213@gmail.com>
2019-03-31 16:55:17 +0530

    2nd commit

---
+++ app/src/main/java/com/example/encore2019/cultural_events/vogue.java
@@ -0,0 +1,24 @@
+package com.example.encore2019.cultural_events;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.DisplayMetrics;
+
+import com.example.encore2019.R;
+
+public class vogue extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vogue);
+
+        DisplayMetrics dm= new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+
+        int width=dm.widthPixels;
+        int height=dm.heightPixels;
+
+        getWindow().setLayout((int)(width*.8),(int)(height*.8));
+    }
+}
